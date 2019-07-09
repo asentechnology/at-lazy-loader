b6f91c4bd288bcf5a363046ba65add7e5f9e7e3a
Sean McManus <mcmanussean@gmail.com>
2019-07-09 05:21:06 -0400

    improves sorting

--- src/js/at-lazy-loader.js
+++ src/js/at-lazy-loader.js
@@ -2,87 +2,36 @@ var Promise = require('es6-promise-polyfill').Promise
 
 class LazyLoadingImages {
   constructor () {
-    this.images = {
-      groupOne: [],
-      groupTwo: [],
-      groupThree: [],
-      groupFour: []
-    }
+    this.images = []
 
     this.sortImages()
-
-    this.loadGroupOneImages().then(() => {
-      this.loadGroupTwoImages().then(() => {
-        this.loadGroupThreeImages().then(() => {
-          this.loadGroupFourImages()
-        })
-      })
-    })
   }
 
   sortImages () {
+    let sortedImages = {}
+
     const viewportHeight = window.innerHeight
+
     const images = document.getElementsByTagName('img')
-
     for (var i = 0; i < images.length; i++) {
-      const position = images[i].getBoundingClientRect()
+      const position = Math.floor(
+        Math.abs(images[i].getBoundingClientRect().y / 1000)
+      )
 
-      switch (true) {
-        case (position.bottom > 0 && position.bottom < viewportHeight) ||
-          (position.top > 0 && position.top < viewportHeight):
-          this.images.groupOne.push(images[i])
-          break
-
-        case (-viewportHeight < position.bottom &&
-          position.bottom < viewportHeight * 2) ||
-          (-viewportHeight < position.top && position.top < viewportHeight * 2):
-          this.images.groupTwo.push(images[i])
-          break
-
-        case (-viewportHeight * 2 < position.bottom &&
-          position.bottom < viewportHeight * 3) ||
-          (-viewportHeight * 2 < position.top &&
-            position.top < viewportHeight * 3):
-          this.images.groupThree.push(images[i])
-          break
-
-        default:
-          this.images.groupFour.push(images[i])
-      }
+      sortedImages[position] = sortedImages[position] || []
+      sortedImages[position].push(images[i])
     }
+
+    this.images = Object.values(sortedImages)
   }
 
-  loadGroupOneImages () {
-    let promises = []
-    for (var i = 0; i < this.images.groupOne.length; i++) {
-      promises.push(this.loadImage(this.images.groupOne[i]))
-    }
-    return Promise.all(promises)
-  }
-
-  loadGroupTwoImages () {
-    let promises = []
-    for (var i = 0; i < this.images.groupTwo.length; i++) {
-      promises.push(this.loadImage(this.images.groupTwo[i]))
-    }
-    return Promise.all(promises)
-  }
-
-  loadGroupThreeImages () {
-    let promises = []
-    for (var i = 0; i < this.images.groupThree.length; i++) {
-      promises.push(this.loadImage(this.images.groupThree[i]))
-    }
-    return Promise.all(promises)
-  }
-
-  loadGroupFourImages () {
-    let promises = []
-    for (var i = 0; i < this.images.groupFour.length; i++) {
-      promises.push(this.loadImage(this.images.groupFour[i]))
-    }
-    return Promise.all(promises)
-  }
+  // loadGroupOneImages () {
+  //   let promises = []
+  //   for (var i = 0; i < this.images.groupOne.length; i++) {
+  //     promises.push(this.loadImage(this.images.groupOne[i]))
+  //   }
+  //   return Promise.all(promises)
+  // }
 
   loadImage (image) {
     return new Promise((resolve, reject) => {
