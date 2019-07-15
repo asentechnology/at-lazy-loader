826b6252b116796f76f6dec3f238a66ffdf296ae
Sean McManus <mcmanussean@gmail.com>
2019-07-15 00:45:46 -0400

    fixes minor bug

--- src/js/at-lazy-loader.js
+++ src/js/at-lazy-loader.js
@@ -17,6 +17,7 @@ class LazyLoadingImages {
     const viewportHeight = window.innerHeight
 
     const images = document.querySelectorAll('[data-at-lazy-load-src]')
+
     for (var i = 0; i < images.length; i++) {
       const position = Math.floor(
         Math.abs(images[i].getBoundingClientRect().y / 1000)
@@ -30,24 +31,24 @@ class LazyLoadingImages {
   }
 
   loadImages (group) {
-    let promises = []
+    if (this.images[group]) {
+      let promises = []
 
-    const groupImages = this.images[group]
+      const groupImages = this.images[group]
 
-    for (var i = 0; i < groupImages.length; i++) {
-      promises.push(this.loadImage(groupImages[i]))
-    }
-
-    Promise.all(promises).then(() => {
-      console.log(
-        'group ' + group + ' loaded ',
-        (new Date().getTime() - this.start) / 1000
-      )
-
-      if (this.images[group + 1]) {
-        this.loadImages(group + 1)
+      for (var i = 0; i < groupImages.length; i++) {
+        promises.push(this.loadImage(groupImages[i]))
       }
-    })
+
+      Promise.all(promises).then(() => {
+        console.log(
+          'group ' + group + ' loaded ',
+          (new Date().getTime() - this.start) / 1000
+        )
+
+        this.loadImages(group + 1)
+      })
+    }
   }
 
   loadImage (image) {
