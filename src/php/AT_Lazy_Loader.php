c04eb2b069cbeb9ad1399dea92c50b09b90b83fc
Sean McManus <mcmanussean@gmail.com>
2019-07-21 13:14:38 -0400

    adds settings page

--- src/php/AT_Lazy_Loader.php
+++ src/php/AT_Lazy_Loader.php
@@ -8,6 +8,7 @@ class AT_Lazy_Loader
   {
     add_action('wp_enqueue_scripts', array(__CLASS__, 'load_script'));
     add_action('admin_init', array(__CLASS__, 'register_settings'));
+    add_action('admin_menu', array(__CLASS__, 'register_options_page'));
 
     self::load_loaders();
   }
@@ -27,9 +28,47 @@ class AT_Lazy_Loader
   {
     add_option('at_lazy_loader_image_placeholder', 'blank');
     register_setting(
-      'at_lazy_loader_images',
+      'at_lazy_loader_settings',
       'at_lazy_loader_image_placeholder'
     );
+  }
+
+  static function register_options_page()
+  {
+    add_options_page(
+      'AT Lazy Loader',
+      'AT Lazy Loader',
+      'manage_options',
+      'at_lazy_loader_menu_slug',
+      array(__CLASS__, 'at_lazy_loader_options_page')
+    );
+  }
+
+  static function at_lazy_loader_options_page()
+  {
+    $blank_selected =
+      get_option('at_lazy_loader_image_placeholder') == 'Blank'
+        ? 'selected'
+        : '';
+    $low_res_image_selected =
+      get_option('at_lazy_loader_image_placeholder') == 'Low Res Image'
+        ? 'selected'
+        : '';
+    ?>
+      <div>
+        <h1>AT Lazy Loader</h1>
+        <h3>Image Settings</h3>
+        <form method="post" action="options.php">
+          <?php settings_fields('at_lazy_loader_settings'); ?>
+          <label for="at_lazy_loader_image_placeholder">Image Placeholder:</label>
+          <select id="at_lazy_loader_image_placeholder" name="at_lazy_loader_image_placeholder">
+            <option <?= $blank_selected ?>>Blank</option>
+            <option <?= $low_res_image_selected ?>>Low Res Image</option>
+          </select>
+          <?php submit_button(); ?>
+        </form>
+      </div>
+    <?php
   }
 
   static function load_loaders()
